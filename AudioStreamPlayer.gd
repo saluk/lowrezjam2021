8a599c052fd2b18313c3d0a8a4dfc69903f8c1e5
Patrick Mullen <patrickm@surveymonkey.com>
2021-08-01 17:10:42 -0700

    Configuring export for html5

--- AudioStreamPlayer.gd
+++ AudioStreamPlayer.gd
@@ -5,23 +5,29 @@ extends AudioStreamPlayer
 # var a = 2
 # var b = "text"
 
+var intro_music = [
+	'aderin-pursuit',
+	'alexander-nakarada-psychotic',
+	'arthur-vyncke-uncertainty',
+	'batchbug-flesh-and-bones',
+	'fm-freemusic-regret-and-sadness',
+	'pratzapp-aadhithya-ambush',
+	'pyrosion-trust',
+	'scott-buckley-beyond-these-walls',
+	'yoitrax-dark-web',
+	'yoitrax-lonely-streets'
+]
 
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
+	random_song()
+	play()
+	
+func random_song():
 	randomize()
-	var d = Directory.new()
-	if d.open("res://music") == OK:
-		d.list_dir_begin()
-		var music_files = []
-		var file_name = d.get_next()
-		while file_name != "":
-			if file_name.ends_with(".mp3"):
-				music_files.append(file_name)
-			file_name = d.get_next()
-		print(music_files)
-		music_files.shuffle()
-		stream = ResourceLoader.load("res://music/" + music_files[0])
-		play()
+	intro_music.shuffle()
+	stream = ResourceLoader.load("music/" + intro_music[0] + ".mp3")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
